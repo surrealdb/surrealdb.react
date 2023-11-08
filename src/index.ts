e5c31588f32e40ad6cbfa447e358f986089f7bc7
Micha de Vries <micha@devrie.sh>
2023-11-08 16:00:58 +0100

    Expose all hooks

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ export {
     type CacheValue,
 } from './cache';
 export { SurrealClient } from './client';
+export { SurrealContext, SurrealProvider } from './provider';
+
+export { useAuthenticate } from './methods/useAuthenticate';
+export { useCreate } from './methods/useCreate';
+export { useDelete } from './methods/useDelete';
+export { useInfo } from './methods/useInfo';
+export { useInvalidate } from './methods/useInvalidate';
+export { useMerge } from './methods/useMerge';
 export { useQuery } from './methods/useQuery';
 export { useQueryMutation } from './methods/useQueryMutation';
 export { useSelect } from './methods/useSelect';
-export { SurrealContext, SurrealProvider } from './provider';
+export { useSignin } from './methods/useSignin';
+export { useSignup } from './methods/useSignup';
+export { useUpdate } from './methods/useUpdate';
