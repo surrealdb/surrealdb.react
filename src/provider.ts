4a446e61b46df2b178a6f9b6b3b35ee154f5c27c
Micha de Vries <micha@devrie.sh>
2023-11-08 16:00:50 +0100

    memoize the surreal client

--- src/provider.ts
+++ src/provider.ts
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, createContext, createElement } from 'react';
+import { ReactNode, createContext, createElement, useMemo } from 'react';
 import { SurrealClient } from './client';
 
 export const SurrealContext = createContext<SurrealClient | undefined>(
@@ -13,5 +13,6 @@ export function SurrealProvider({
     children?: ReactNode;
     client: SurrealClient;
 }) {
-    return createElement(SurrealContext.Provider, { value: client }, children);
+    const value = useMemo(() => client, []);
+    return createElement(SurrealContext.Provider, { value }, children);
 }
