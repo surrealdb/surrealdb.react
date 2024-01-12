c243d7af4b40ce9ae749ae5e361df9e9c54fbc21
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-01-12 12:02:07 +0200

    test(useInfo): Linting fixes

--- src/__ tests __/methods/useInfo.spec.tsx
+++ src/__ tests __/methods/useInfo.spec.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import { renderHook } from '@testing-library/react';
 import { SurrealClient } from '@/client';
-import { SurrealProvider } from '@/provider';
+import { useAbstractQuery } from '@/methods/useAbstract';
 import { useInfo } from '@/methods/useInfo';
+import { SurrealProvider } from '@/provider';
+import { renderHook } from '@testing-library/react';
+import React from 'react';
 
 jest.mock('@/methods/useAbstract', () => ({
-  useAbstractQuery: jest.fn(),
+    useAbstractQuery: jest.fn().mockImplementation(() => {}),
 }));
 
 describe('useInfo', () => {
-  it('should call useAbstractQuery with correct parameters', () => {
-    const mockUseAbstractQuery = require('@/methods/useAbstract').useAbstractQuery;
-    const surrealClient = new SurrealClient();
-    const queryKey = ['__auth', 'info'];
-    const abstractArguments = {};
+    it('should call useAbstractQuery with correct parameters', () => {
+        const surrealClient = new SurrealClient();
+        const queryKey = ['__auth', 'info'];
+        const abstractArguments = {};
 
-    const wrapper: React.FC<{children: React.ReactNode}> = ({ children }) => (
-      <SurrealProvider client={surrealClient}>
-        {children}
-      </SurrealProvider>
-    );
+        const wrapper: React.FC<{ children: React.ReactNode }> = ({
+            children,
+        }) => (
+            <SurrealProvider client={surrealClient}>{children}</SurrealProvider>
+        );
 
-    renderHook(() => useInfo(abstractArguments), {
-      wrapper,
+        renderHook(() => useInfo(abstractArguments), {
+            wrapper,
+        });
+
+        expect(useAbstractQuery).toHaveBeenCalledWith(
+            JSON.stringify(queryKey),
+            expect.any(Function),
+            abstractArguments
+        );
     });
-
-    expect(mockUseAbstractQuery).toHaveBeenCalledWith(
-      JSON.stringify(queryKey),
-      expect.any(Function),
-      abstractArguments
-    );
-  });
-
 });
-
-
-
