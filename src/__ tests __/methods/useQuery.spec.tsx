25ce2b5ea294d05d9ce2dc27a460a329b3aca22b
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-02-14 12:56:54 +0000

    test(useInvalidate): Linting fixes

--- src/__ tests __/methods/useQuery.spec.tsx
+++ src/__ tests __/methods/useQuery.spec.tsx
@@ -1,42 +1,59 @@
+import { MemoryCache } from '@/cache';
 import { SurrealClient } from '@/client';
 import { useQuery } from '@/methods/useQuery';
 import { SurrealContext } from '@/provider';
 import { renderHook } from '@testing-library/react';
-import { MemoryCache } from '@/cache';
-import { Surreal } from 'surrealdb.js';
 import React from 'react';
+import { Surreal } from 'surrealdb.js';
 
 jest.mock('@/library/fetcherFactory', () => ({
-  fetcherFactory: jest.fn().mockImplementation(() => jest.fn().mockResolvedValue({ data: 'mock data' })),
+    fetcherFactory: jest
+        .fn()
+        .mockImplementation(() =>
+            jest.fn().mockResolvedValue({ data: 'mock data' })
+        ),
 }));
 
 jest.mock('@/methods/useAbstract', () => ({
-  useAbstractQuery: jest.fn().mockImplementation(() => ({
-    data: 'mock data', isLoading: false, error: null, isPending: false, isError: false, isSuccess: false, isFetching: false, isIdle: true, refetch: jest.fn(), status: 'pending', fetchStatus: 'idle', responseUpdatedAt: undefined,
-  })),
+    useAbstractQuery: jest.fn().mockImplementation(() => ({
+        data: 'mock data',
+        isLoading: false,
+        error: null,
+        isPending: false,
+        isError: false,
+        isSuccess: false,
+        isFetching: false,
+        isIdle: true,
+        refetch: jest.fn(),
+        status: 'pending',
+        fetchStatus: 'idle',
+        responseUpdatedAt: undefined,
+    })),
 }));
 
 const mockClient = new SurrealClient({
-  cache: new MemoryCache(),
-  surreal: new Surreal(),
+    cache: new MemoryCache(),
+    surreal: new Surreal(),
 });
 
 const wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <SurrealContext.Provider value={mockClient}>
-      {children}
-  </SurrealContext.Provider>
+    <SurrealContext.Provider value={mockClient}>
+        {children}
+    </SurrealContext.Provider>
 );
 
 describe('useQuery', () => {
-  it('should call useQuery with correct parameters', async () => {
-    const { result } = renderHook(() => useQuery({
-      queryKey: ['exampleQueryKey'],
-      query: 'SELECT * FROM exampleTable',
-      queryBindings: { exampleBinding: 'value' },
-    }), { wrapper });
-  
-    expect(result.current).toBeDefined();
+    it('should call useQuery with correct parameters', async () => {
+        const { result } = renderHook(
+            () =>
+                useQuery({
+                    queryKey: ['exampleQueryKey'],
+                    query: 'SELECT * FROM exampleTable',
+                    queryBindings: { exampleBinding: 'value' },
+                }),
+            { wrapper }
+        );
 
-  });
+        expect(result.current).toBeDefined();
+    });
 });
-
