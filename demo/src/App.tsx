a5613bdfa542a7b524b866fb2f1bca9e97f542d1
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-03-28 14:26:47 +0000

    feat(hooks-demo): It adds scope definitions and code snippet highliter

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,6 +7,8 @@ import QueryComponent from './components/QueryComponent';
 import SigninComponent from './components/SigninComponent';
 import SignupComponent from './components/SignupComponent';
 import UpdateComponent from './components/UpdateComponent';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './styles.css';
 
 const components = [
@@ -22,28 +24,57 @@ const components = [
 
 export default function App() {
     const [activeComponentIndex, setActiveComponentIndex] = useState(0);
+    const [showParagraph, setShowParagraph] = useState(true);
 
     const navigateLeft = () => {
         setActiveComponentIndex((prevIndex) =>
             prevIndex === 0 ? components.length - 1 : prevIndex - 1
         );
+        setShowParagraph(false);
     };
 
     const navigateRight = () => {
         setActiveComponentIndex((prevIndex) =>
             prevIndex === components.length - 1 ? 0 : prevIndex + 1
         );
+        setShowParagraph(false);
     };
 
     const handleSelectChange = (e) => {
         setActiveComponentIndex(Number(e.target.value));
+        setShowParagraph(false); 
     };
 
     const ActiveComponent = components[activeComponentIndex].component;
 
+    const codeString = `
+    surreal start --auth --user root --pass root
+
+    surreal sql -u root -p root --namespace test --database test -e ws://0.0.0.0:8000
+
+    DEFINE SCOPE account SESSION 24h
+    SIGNUP ( CREATE user SET email = $email, pass = crypto::argon2::generate($pass) )
+    SIGNIN ( SELECT * FROM user WHERE email = $email AND crypto::argon2::compare(pass, $pass) )
+    ;
+
+    DEFINE TABLE demo SCHEMALESS PERMISSIONS FULL;
+
+    DEFINE INDEX unique_email ON user FIELDS email UNIQUE;
+    DEFINE INDEX unique_username ON user FIELDS username UNIQUE;`;
+
     return (
         <div className="app-container">
             <h2>SurrealDB React Hooks Demo</h2>
+
+            {showParagraph && (
+                <div className="code-container">
+                <h3>Run the following commands in your terminal</h3>
+                <SyntaxHighlighter language="bash" style={materialLight}>
+                    {codeString}
+                </SyntaxHighlighter>
+                </div>
+            )}
+
             <div className="navigation">
                 <button onClick={navigateLeft}>{'<'}</button>
                 <select
